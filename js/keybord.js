1e33abfba5accf0b651e7fd343f57f7f5b264f50
hant <ishant@qq.com>
2021-04-01 16:43:46 +0800

    界面

--- js/keybord.js
+++ js/keybord.js
@@ -1,9 +1,54 @@
 $(function () {
-  document.body.addEventListener('keydown',function (e) {
-    $('#' + e.code).addClass('active')
+  var luck = {}
+  document.body.addEventListener('keypress',function (e) {
+    if (luck[e.code] === undefined){
+      $('#' + e.code).addClass('active')
+      luck[e.code] = 1
+      console.log(1)
+    }
   })
   document.body.addEventListener('keyup',function (e) {
-    $('#' + e.code).removeClass('active')
+    luck[e.code] = undefined
+    setTimeout(function () {
+      $('#' + e.code).removeClass('active')
+    },700)
+  })
+  $('#showWord').click(function () {
+    var attr = $(this).attr('ishow')
+    if (attr==='true'){
+      $('.word').addClass('show_active')
+      $(this).attr('ishow','false')
+      $(this).html('不显示字母')
+    }else {
+      $('.word').removeClass('show_active')
+      $(this).attr('ishow','true')
+      $(this).html('显示字母')
+    }
+  })
+
+  $('#showMusic').click(function () {
+    var attr = $(this).attr('ishow')
+    if (attr==='true'){
+      $('.music').addClass('show_active')
+      $(this).attr('ishow','false')
+      $(this).html('不显示音阶')
+    }else {
+      $('.music').removeClass('show_active')
+      $(this).attr('ishow','true')
+      $(this).html('显示音阶')
+    }
+  })
+  $('#showDo').click(function () {
+    var attr = $(this).attr('ishow')
+    if (attr==='true'){
+      $('.do').addClass('show_active')
+      $(this).attr('ishow','false')
+      $(this).html('不显示Do')
+    }else {
+      $('.do').removeClass('show_active')
+      $(this).attr('ishow','true')
+      $(this).html('显示Do')
+    }
   })
 })
 
